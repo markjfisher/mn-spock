76635c122626ae0ce4ba4b21ff44996d72d4de0b
mark.fisher <mark.fisher@e2x.co.uk>
2018-11-28 16:13:32 +0000

    class name typo

--- mn-spock-1.0.1/src/test/groovy/mn/SpockSpec.groovy
+++ mn-spock-1.0.1/src/test/groovy/mn/SpockSpec.groovy
@@ -4,7 +4,7 @@ import io.micronaut.test.annotation.MicronautTest
 import spock.lang.Specification
 
 @MicronautTest
-class SpocSpeck extends Specification {
+class SpockSpeck extends Specification {
     def 'it should compile'() {
         expect:
         true
